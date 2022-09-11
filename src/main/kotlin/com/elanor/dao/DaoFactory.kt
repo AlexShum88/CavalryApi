45f7a5f8b03d924696f0c974914736c9cf471b02
Shum Olexandr <l1.shum.pavl@gmail.com>
2022-09-11 17:43:54 +0300

    try 9

--- src/main/kotlin/com/elanor/dao/DaoFactory.kt
+++ src/main/kotlin/com/elanor/dao/DaoFactory.kt
@@ -9,16 +9,16 @@ import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransacti
 
 object DaoFactory {
     fun init() {
-//        val driverClassName = "org.postgresql.Driver"
-//        val jdbcURL = "jdbc:postgresql://localhost:5432/cavalry_db"
-//        val username = "postgres"
-//        val password = "root"
+        val driverClassName = "org.postgresql.Driver"
+        val jdbcURL = "jdbc:postgresql://ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/d48eh5bdptjufe"
+        val username = "cgaxplsywoxkii"
+        val password = "42e75946d98a0862c53e15dc3b6d825c9c4f7758d99b15ac032d4e444dae920d"
 
-//        val database = Database.connect(jdbcURL, driverClassName, user = username,
-//            password =  password)
-        val config = HikariConfig("hikari.properties")
-        val dataSource = HikariDataSource(config)
-        Database.connect(dataSource)
+        val database = Database.connect(jdbcURL, driverClassName, user = username,
+            password =  password)
+//        val config = HikariConfig("hikari.properties")
+//        val dataSource = HikariDataSource(config)
+//        Database.connect(dataSource)
 
     }
 
